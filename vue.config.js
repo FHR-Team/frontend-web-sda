ab1bf811bfa5851fa7172d6ac7bf08c7e7a067e7
harisfi <acc.cp.haris@gmail.com>
2021-08-05 20:49:14 +0700

    Update production source map

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    productionSourceMap: false,
+    productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
     publicPath: process.env.NODE_ENV === 'production'
     ? '/frontend-web-sda/'
     : '/'
